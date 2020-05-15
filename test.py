fd2afaa3c21dd5673be598818396a5be2940260e
Brian Yau <brianyauu@gmail.com>
2020-05-15 20:23:51 +0800

    mv test to main file

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import unittest
 import requests
 import json
+domain_name = "https://briyana.ddns.net"
+student_session = "eyJ1c2VyIjoiMSJ9.Xre7gg.0LpCnpVsmPJ082hwygo0BXXBRpM"
+teacher_session = "eyJ1c2VyIjoiMiJ9.Xr6AWw.AsyKDK77KzZe4NGGiBD1inHxbFI"
 
 class FYPUnitTest(unittest.TestCase):
     def test_login(self):
         # Using user_id and password to Login and get the result
         login_data = {'user_id': 1, 'password': 'Student01'}
-        request_result = requests.post("https://briyana.ddns.net/login_process", data=login_data, verify=False)
+        request_result = requests.post(domain_name+"/login_process", data=login_data, verify=False)
 
         # Expected: Get index page after login success
         with open('static/index.html') as f:
@@ -15,12 +18,12 @@ class FYPUnitTest(unittest.TestCase):
         self.assertEqual(request_result.text, login_result, "Login Fail")
 
     def test_user_info(self):
-        cookies = { 'session': "eyJ1c2VyIjoiMSJ9.Xre7gg.0LpCnpVsmPJ082hwygo0BXXBRpM" }
-        request_with_session = requests.get("https://briyana.ddns.net/api/user_data", cookies=cookies, verify=False)
-        request_without_session = requests.get("https://briyana.ddns.net/api/user_data", verify=False)
+        cookies = { 'session': student_session }
+        request_with_session = requests.get(domain_name+"/api/user_data", cookies=cookies, verify=False)
+        request_without_session = requests.get(domain_name+"/api/user_data", verify=False)
 
         # Expected: User Information of User 1
-        user_info = {
+        expected = {
             "user_data": {
                 "name": "wong tai man",
                 "nickname": "peter",
@@ -35,17 +38,52 @@ class FYPUnitTest(unittest.TestCase):
             "result": "Error"
         }
 
-        self.assertEqual(json.loads(request_with_session.text), user_info, "Get User Information Fail")
+        self.assertEqual(json.loads(request_with_session.text), expected, "Get User Information Fail")
         self.assertEqual(json.loads(request_without_session.text), error_msg, "Get User Information Fail")
 
-    def test_b_inf(self):
-        pass
+    def test_changePW(self):
+        cookies = { 'session': teacher_session }
+        data = {
+            "changePassword": {
+               "password_now": "teacher1",
+               "password_new": "Teacher01"
+            }
+        }
+        request_result = requests.put(domain_name+"/api/user_data", json=data, cookies=cookies, verify=False)
 
-    def test_cs(self):
-        pass
+        # Expected: Success to change password of User 2
+        expected = {
+            "changePassword": "Success"
+        }
 
-    def test_df(self):
-        pass
+        self.assertEqual(json.loads(request_result.text), expected, "Error to change password")
+
+        # Change it back after test
+        data = {
+            "changePassword": {
+               "password_now": "Teacher01",
+               "password_new": "teacher1"
+            }
+        }
+        request_result = requests.put(domain_name+"/api/user_data", json=data, cookies=cookies, verify=False)
+
+    def test_close_task(self):
+        cookies = { 'session': teacher_session }
+        expected = {'force_close_task': 'Success'}
+
+        request_result = requests.patch(domain_name+"/api/task?c=1&t=10", cookies=cookies, verify=False)
+
+        self.assertEqual(json.loads(request_result.text), expected, "Error to close task")
+
+    def test_upload_icon(self):
+        cookies = { 'session': student_session }
+        files = {'icon': open('static/images/user_icons/2.png', 'rb')}
+        expected = {'changeIcon': 'Success'}
+
+        request_result = requests.post(domain_name+"/api/user_info", files=files, cookies=cookies, verify=False)
+
+
+        self.assertEqual(json.loads(request_result.text), expected, "Error to upload icon")
 
 if __name__ == '__main__':
     unittest.main()
